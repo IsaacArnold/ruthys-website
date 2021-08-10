072d20ac416d92469c30205e356657f95638584e
Isaac Arnold <isaacarnold@icloud.com>
2021-08-10 20:45:45 +1200

    Adds media query styles
    Also adds vh fix for the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { use100vh } from "react-div-100vh";
 import { StaticImage } from "gatsby-plugin-image";
-
 import Scrollwheel from "../images/svg/scrollwheel.svg";
 
 const Hero = () => {
+  const height = use100vh();
+  const newHeight = height ? height * 0.88 : "88vh";
+
   return (
     <header className="hero">
-      <div className="hero-grid">
+      <div className="hero-grid" style={{ height: newHeight }}>
         <StaticImage
           src="../images/people.jpeg"
           alt="Image of people laughing and sipping coffee inside Ruthy's Cafe Tauranga"
