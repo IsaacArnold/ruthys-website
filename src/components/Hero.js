9e2513a472ecdc987e76dca577093fe0ae49cb07
Isaac Arnold <isaacarnold@icloud.com>
2021-08-16 19:35:44 +1200

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,8 +19,8 @@ const Hero = () => {
         <div className="hero-content content-container">
           <div className="hero-content-txt">
             <h1>
-              Welcome to Ruthy's! Tauriko's favourite cafe for home-style eats,
-              great coffee and even better banter.
+              Welcome to Ruthy's Cafe & Eatery! Tauriko's favourite cafe for
+              home-style eats, great coffee and even better banter.
             </h1>
           </div>
           <div className="scrollwheel">
