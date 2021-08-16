861001143a63f048dfd884705ab19cf01d35e6b5
Isaac Arnold <isaacarnold@icloud.com>
2021-08-16 20:59:39 +1200

    Fixing styles on menu page

--- src/pages/menus.js
+++ src/pages/menus.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { use100vh } from "react-div-100vh";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/Layout";
@@ -10,16 +9,10 @@ import "../styles/styles.scss";
 import "../styles/menus.scss";
 
 const Menus = () => {
-  const height = use100vh();
-  const newHeight = height ? height * 0.88 : "88vh";
-
   return (
     <Layout>
       <Seo title="Ruthy's Menus" description="Menu page description" />
-      <section
-        className="menus-grid content-container"
-        style={{ height: newHeight }}
-      >
+      <section className="menus-grid content-container">
         <div className="menus-content">
           <h2>Menus</h2>
           <p>
