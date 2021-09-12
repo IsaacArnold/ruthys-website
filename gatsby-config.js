b2318d26e4fff3f2a3fd376abdeddf8a443bb8c0
Isaac Arnold <isaacarnold@icloud.com>
2021-09-12 20:36:04 +1200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,6 @@ module.exports = {
     titleTemplate: "%s | Ruthy's Coffee Bar & Eatery Tauranga",
     description: `Ruthy's Coffee Bar & Eatery is a popular Tauranga Cafe located in Tauriko. Open for breakfast and lunch serving delicious cabinet and kitchen food with amazing Excelso espresso.`,
     siteUrl: `https://ruthyscafe.netlify.app/`,
+    image: "./src/images/people.jpeg",
   },
 };
